a2d9f3530e34af523d3a4e156e21acc30ed65fbb
Brariv <119123309+Brariv@users.noreply.github.com>
2024-02-20 15:25:23 -0600

    Upload old code

---
+++ src/AppTest.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    private DevelopedStack stack = new DevelopedStack();
+
+    @Test
+    public void testApp() {
+        Txtreader txtreader = new Txtreader();
+        assertEquals(txtreader.readTxTFile("prueba.txt").get(0), "6");
+        assertEquals(txtreader.readTxTFile("prueba.txt").get(1), "2");
+        assertEquals(txtreader.readTxTFile("prueba.txt").get(2), "3");
+        assertEquals(txtreader.readTxTFile("prueba.txt").get(3), "+");
+    }
+
+    @Test
+    public void testPushAndTop() {
+        stack.push(1);
+        assertEquals(1, stack.top());
+        stack.push(2);
+        assertEquals(2, stack.top());
+        stack.push(3);
+        assertEquals(3, stack.top());
+    }
+
+    @Test
+    public void testPop() {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assertEquals(2, stack.pop(2));
+        assertEquals(1, stack.pop(1));
+        assertEquals(null, stack.pop(0));
+        assertNull(stack.pop(0));
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(stack.isEmpty());
+        stack.push(1);
+        assertFalse(stack.isEmpty());
+        stack.pop(0);
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testTopOnEmptyStack() {
+        assertNull(stack.top());
+    }
+
+    @Test
+    public void testPopOnEmptyStack() {
+        assertNull(stack.pop(0));
+    }
+    
+}
---
+++ src/DevelopedStack.java
@@ -0,0 +1,96 @@
+
+import java.util.Vector;
+
+/**
+ * Esta clase representa una implementación de la interfaz UVGStack utilizando un Vector como estructura de datos subyacente.
+ * Permite realizar operaciones básicas de una pila, como agregar elementos, quitar elementos, obtener el elemento en la cima y verificar si la pila está vacía.
+ */
+public class DevelopedStack implements UVGStack{
+
+    Vector<Object> stack = new Vector<Object>();
+
+    /**
+     * Agrega un elemento a la cima de la pila.
+     * @param item el elemento a agregar
+     */
+    @Override
+    public void push(Object item) {
+        stack.add(item);
+    }
+
+    /**
+     * Elimina y devuelve el elemento en la cima de la pila.
+     * @return el elemento en la cima de la pila, o null si la pila está vacía
+     */
+    @Override
+    public Object pop(int index) {
+        if (isEmpty()){
+            return null;
+        }else if (stack.size() == 1){
+            stack.removeElementAt(0);
+            return null;
+        }
+        stack.removeElementAt(index);
+        return top();
+    }
+
+    /**
+     * Devuelve el elemento en la cima de la pila sin eliminarlo.
+     * @return el elemento en la cima de la pila, o null si la pila está vacía
+     */
+    @Override
+    public Object top() {
+        if (isEmpty()){
+            return null;
+        }
+
+        return stack.elementAt(stack.size() - 1 );
+    }
+
+    /**
+     * Verifica si la pila está vacía.
+     * @return true si la pila está vacía, false de lo contrario
+     */
+    @Override
+    public boolean isEmpty() {
+        if (stack.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    /**
+     * Devuelve el indice deseado de la pila.
+     * @return el indice deseado de la pila, o null si la pila está vacía
+     */
+    public Object getIndex(int index){
+        if (isEmpty()){
+            return null;
+        }
+        return stack.elementAt(index);
+    }
+
+    /**
+     * Devuelve el tamaño de la pila.
+     * @return el tamaño de la pila
+     */
+    public int size(){
+        return stack.size();
+    }
+
+    /**
+     * Reemplaza el elemento en el indice deseado de la pila.
+     * @return el elemento reemplazado de la pila, o null si la pila está vacía
+     */
+    public Object replace(int index, Object item){
+        if (isEmpty()){
+            return null;
+        }
+        stack.set(index, item);
+        return stack.elementAt(index);
+    }
+}
+
