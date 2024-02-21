1de3a94f44a4d805ba76dcdc5a4cdcb866f9ba7a
JavierCC <cif23079@uvg,edu.gt>
2024-02-20 20:11:43 -0600

    Infix y factory ver1

--- src/infix.java
+++ src/infix.java
@@ -1,3 +1,11 @@
-public class infix {
-    
+public class Infix {
+    public void someMethod() {
+            DevelopedStack newStack = new DevelopedStack();
+            StringBuilder postfixExpression = new StringBuilder();
+            while (!newStack.isEmpty()) {
+                postfixExpression.insert(0, newStack.pop(0) + " ");
+            }
+            System.out.println("Expresión en formato postfix: " + postfixExpression.toString().trim());
+        
+    }
 }
